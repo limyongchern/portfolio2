f7f45dec161a441b6b0d07fc42699dd28e1140a7
yongchernlim <yongchern.lim@v.systems>
2024-06-24 02:13:39 +0800

    header color change

--- components/navigation/header.tsx
+++ components/navigation/header.tsx
@@ -2,13 +2,14 @@ import {
   Flex,
   Header as HeaderMantine,
   HeaderProps,
+  Menu,
   Stack,
   UnstyledButton,
 } from '@mantine/core';
 import { TbMenu2 } from 'react-icons/tb';
 import { useState } from 'react';
 import styles from 'styles/components/header.module.scss';
-import { Heading } from '../typography';
+import { Body, Heading } from '../typography';
 import { sections } from './sidebar';
 import NavLink, { ILink } from './navLink';
 import Link from 'next/link';
@@ -52,33 +53,96 @@ const Header = (props: IProps) => {
           </Link>
         </Flex>
         <div className={styles.nav}>
-          <UnstyledButton>
-            <Heading variant={4} color="white">
-              For Me
+          <UnstyledButton
+            onClick={() => router.push('/')}
+            className={`${styles.link} ${
+              router.pathname === '/' ? styles.active : ''
+            }`}>
+            <Heading
+              variant={4}
+              className={`${router.pathname === '/' ? styles.active : ''}`}>
+              Home{' '}
             </Heading>
           </UnstyledButton>
-          <UnstyledButton>
-            <Heading variant={4} color="white">
+          <UnstyledButton
+            onClick={() => router.push('/about')}
+            className={`${styles.link} ${
+              router.pathname === '/about' ? styles.active : ''
+            }`}>
+            <Heading
+              variant={4}
+              // color="white"
+              className={`${
+                router.pathname === '/about' ? styles.active : ''
+              }`}>
               About Us{' '}
             </Heading>
           </UnstyledButton>
-          <UnstyledButton onClick={() => router.push('/whitepaper')}>
-            <Heading variant={4} color="white">
+          <UnstyledButton
+            onClick={() => router.push('/whitepaper')}
+            className={`${styles.link} ${
+              router.pathname === '/whitepaper' ? styles.active : ''
+            }`}>
+            <Heading
+              variant={4}
+              // color="white"
+              className={`${
+                router.pathname === '/whitepaper' ? styles.active : ''
+              }`}>
               Whitepaper{' '}
             </Heading>
           </UnstyledButton>
-          <UnstyledButton onClick={() => router.push('/community')}>
-            <Heading variant={4} color="white">
+          <UnstyledButton
+            onClick={() => router.push('/community')}
+            className={`${styles.link} ${
+              router.pathname === '/community' ? styles.active : ''
+            }`}>
+            <Heading
+              variant={4}
+              // color="white"
+              className={`${
+                router.pathname === '/community' ? styles.active : ''
+              }`}>
               Community{' '}
             </Heading>
           </UnstyledButton>
-          <UnstyledButton>
-            <Heading variant={4} color="white">
+          <UnstyledButton
+            onClick={() => router.push('/download')}
+            className={`${styles.link} ${
+              router.pathname === '/download' ? styles.active : ''
+            }`}>
+            <Heading
+              variant={4}
+              // color="white"
+              className={`${
+                router.pathname === '/download' ? styles.active : ''
+              }`}>
               Download{' '}
             </Heading>
           </UnstyledButton>
 
-          <Translate size={32} color="white" />
+          <Menu>
+            <Menu.Target>
+              <Translate size={32} color="white" />
+            </Menu.Target>
+
+            <Menu.Dropdown className={styles.menu}>
+              <Menu.Item>
+                {
+                  <Body variant={2} color="#4178FA">
+                    English
+                  </Body>
+                }
+              </Menu.Item>
+              <Menu.Item>
+                {
+                  <Body variant={2} color="#F2F3F7">
+                    简体中文
+                  </Body>
+                }
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
         </div>
       </div>
     </HeaderMantine>
