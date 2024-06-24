88b30f329a8566b1f1a836bea70f3a381e83aab8
ZeThong <zethong.lew@v.systems>
2024-06-24 15:52:03 +0800

    fix: fix header max width to 1080

--- components/navigation/header.tsx
+++ components/navigation/header.tsx
@@ -41,108 +41,110 @@ const Header = (props: IProps) => {
         menuOpen ? styles.opened : '',
         router.pathname === '/' ? styles.transparent : styles.normalHeader, // Apply transparent class
       ].join(' ')}>
-      <div className={styles.header}>
-        <Flex gap="md">
-          <UnstyledButton
-            className={styles.hamburger}
-            onClick={() => setMenuOpen((prev) => !prev)}>
-            <TbMenu2 size={32} />
-          </UnstyledButton>
-          <Link href="/">
-            <Image src={LogoSVG} alt="Logo" className={styles.headerlogo} />
-          </Link>
-        </Flex>
-        <div className={styles.nav}>
-          <UnstyledButton
-            onClick={() => router.push('/')}
-            className={`${styles.link} ${
-              router.pathname === '/' ? styles.active : ''
-            }`}>
-            <Heading
-              variant={4}
-              className={`${router.pathname === '/' ? styles.active : ''}`}>
-              Home{' '}
-            </Heading>
-          </UnstyledButton>
-          <UnstyledButton
-            onClick={() => router.push('/about')}
-            className={`${styles.link} ${
-              router.pathname === '/about' ? styles.active : ''
-            }`}>
-            <Heading
-              variant={4}
-              // color="white"
-              className={`${
+      <div className={styles.headerContainer}>
+        <div className={styles.header}>
+          <Flex gap="md">
+            <UnstyledButton
+              className={styles.hamburger}
+              onClick={() => setMenuOpen((prev) => !prev)}>
+              <TbMenu2 size={32} />
+            </UnstyledButton>
+            <Link href="/">
+              <Image src={LogoSVG} alt="Logo" className={styles.headerlogo} />
+            </Link>
+          </Flex>
+          <div className={styles.nav}>
+            <UnstyledButton
+              onClick={() => router.push('/')}
+              className={`${styles.link} ${
+                router.pathname === '/' ? styles.active : ''
+              }`}>
+              <Heading
+                variant={4}
+                className={`${router.pathname === '/' ? styles.active : ''}`}>
+                Home{' '}
+              </Heading>
+            </UnstyledButton>
+            <UnstyledButton
+              onClick={() => router.push('/about')}
+              className={`${styles.link} ${
                 router.pathname === '/about' ? styles.active : ''
               }`}>
-              About Us{' '}
-            </Heading>
-          </UnstyledButton>
-          <UnstyledButton
-            onClick={() => router.push('/whitepaper')}
-            className={`${styles.link} ${
-              router.pathname === '/whitepaper' ? styles.active : ''
-            }`}>
-            <Heading
-              variant={4}
-              // color="white"
-              className={`${
+              <Heading
+                variant={4}
+                // color="white"
+                className={`${
+                  router.pathname === '/about' ? styles.active : ''
+                }`}>
+                About Us{' '}
+              </Heading>
+            </UnstyledButton>
+            <UnstyledButton
+              onClick={() => router.push('/whitepaper')}
+              className={`${styles.link} ${
                 router.pathname === '/whitepaper' ? styles.active : ''
               }`}>
-              Whitepaper{' '}
-            </Heading>
-          </UnstyledButton>
-          <UnstyledButton
-            onClick={() => router.push('/community')}
-            className={`${styles.link} ${
-              router.pathname === '/community' ? styles.active : ''
-            }`}>
-            <Heading
-              variant={4}
-              // color="white"
-              className={`${
+              <Heading
+                variant={4}
+                // color="white"
+                className={`${
+                  router.pathname === '/whitepaper' ? styles.active : ''
+                }`}>
+                Whitepaper{' '}
+              </Heading>
+            </UnstyledButton>
+            <UnstyledButton
+              onClick={() => router.push('/community')}
+              className={`${styles.link} ${
                 router.pathname === '/community' ? styles.active : ''
               }`}>
-              Community{' '}
-            </Heading>
-          </UnstyledButton>
-          <UnstyledButton
-            onClick={() => router.push('/download')}
-            className={`${styles.link} ${
-              router.pathname === '/download' ? styles.active : ''
-            }`}>
-            <Heading
-              variant={4}
-              // color="white"
-              className={`${
+              <Heading
+                variant={4}
+                // color="white"
+                className={`${
+                  router.pathname === '/community' ? styles.active : ''
+                }`}>
+                Community{' '}
+              </Heading>
+            </UnstyledButton>
+            <UnstyledButton
+              onClick={() => router.push('/download')}
+              className={`${styles.link} ${
                 router.pathname === '/download' ? styles.active : ''
               }`}>
-              Download{' '}
-            </Heading>
-          </UnstyledButton>
+              <Heading
+                variant={4}
+                // color="white"
+                className={`${
+                  router.pathname === '/download' ? styles.active : ''
+                }`}>
+                Download{' '}
+              </Heading>
+            </UnstyledButton>
 
-          <Menu>
-            <Menu.Target>
-              <Translate size={32} color="white" />
-            </Menu.Target>
+            <Menu>
+              <Menu.Target>
+                <Translate size={32} color="white" />
+              </Menu.Target>
 
-            <Menu.Dropdown className={styles.menu}>
-              <Menu.Item>
-                {
-                  <Body variant={2} color="#4178FA">
-                    English
-                  </Body>
-                }
-              </Menu.Item>
-              <Menu.Item>
-                {
-                  <Body variant={2} color="#F2F3F7">
-                    简体中文
-                  </Body>
-                }
-              </Menu.Item>
-            </Menu.Dropdown>
-          </Menu>
+              <Menu.Dropdown className={styles.menu}>
+                <Menu.Item>
+                  {
+                    <Body variant={2} color="#4178FA">
+                      English
+                    </Body>
+                  }
+                </Menu.Item>
+                <Menu.Item>
+                  {
+                    <Body variant={2} color="#F2F3F7">
+                      简体中文
+                    </Body>
+                  }
+                </Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+          </div>
         </div>
       </div>
     </HeaderMantine>
