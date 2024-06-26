422536485a943fab05ec5940a2890a9e8d69138a
LauYongJie <yongjie.lau@v.systems>
2024-06-26 14:24:53 +0800

    feat: add header styling for dekstop and mobile

--- components/navigation/header.tsx
+++ components/navigation/header.tsx
@@ -1,8 +1,11 @@
 import {
+  Burger,
+  Divider,
   Flex,
   Header as HeaderMantine,
   HeaderProps,
   Menu,
+  Modal,
   Stack,
   UnstyledButton,
 } from '@mantine/core';
@@ -17,22 +20,67 @@ import Image from 'next/image';
 import LogoSVG from 'public/wolfplanet.svg';
 import { Translate } from '@phosphor-icons/react';
 import { useRouter } from 'next/router';
-
 interface IProps {
   baseProps?: Partial<HeaderProps>;
 }
 
-const links = Object.entries(sections).reduce(
-  (p, c) => [...p, ...c[1]],
-  [] as ILink[]
-);
+const links = [
+  { name: '主页', link: '/' },
+  { name: '关于我们', link: '/about' },
+  { name: '白皮书', link: '/whitepaper' },
+  { name: '社区', link: '/community' },
+  { name: '下载', link: '/download' },
+];
+
+const languages = [{ name: 'English' }, { name: '中文' }];
 
 const Header = (props: IProps) => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [translateClicked, setTranslateClicked] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [active, setActive] = useState(links[0].link);
+  console.log(active, 'hahha');
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 100);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   const router = useRouter();
 
+  const [isMobile, setIsMobile] = useState(false);
+  const [width, setWidth] = useState<number>(
+    typeof window !== 'undefined' ? window.innerWidth : 0
+  );
+  const handleWindowSizeChange = () => {
+    setWidth(typeof window !== 'undefined' ? window.innerWidth : 0);
+  };
+
+  useEffect(() => {
+    if (width <= 835) {
+      setIsMobile(true);
+    } else setIsMobile(false);
+  }, [width]);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', handleWindowSizeChange);
+    }
+    return () => {
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('resize', handleWindowSizeChange);
+      }
+    };
+  }, [width]);
+
+  useEffect(() => {
+    setActive(router.pathname);
+  }, [router.pathname]);
   return (
     <HeaderMantine
       height={'11rem'}
@@ -41,7 +89,9 @@ const Header = (props: IProps) => {
       className={[
         props.baseProps?.className ?? '',
         menuOpen ? styles.opened : '',
-        router.pathname === '/' ? styles.transparent : styles.normalHeader, // Apply transparent class
+        router.pathname === '/' && !isScrolled && !isMobile
+          ? styles.transparent
+          : styles.normalHeader, // Apply transparent class
       ].join(' ')}>
       <div className={styles.headerContainer}>
         <div className={styles.header}>
@@ -147,17 +197,52 @@ const Header = (props: IProps) => {
               </Menu.Dropdown>
             </Menu>
           </div>
-          <UnstyledButton
+          <Burger
+            color="#FFFFFF"
+            opened={menuOpen}
+            onClick={() => setMenuOpen((prev) => !prev)}
+            className={styles.hamburger}
+            size={30}
+          />
+          {/* <UnstyledButton
             className={styles.hamburger}
             onClick={() => setMenuOpen((prev) => !prev)}>
             <TbMenu2 size={24} />
-          </UnstyledButton>
+          </UnstyledButton> */}
         </div>
       </div>
       {menuOpen && (
-        <Stack>
+        <Stack
+          mt={'42px'}
+          align="center"
+          justify="center"
+          spacing={'42px'}
+          className={styles.linkMobile}>
           {links.map((link) => (
-            <NavLink {...link} key={link.label} />
+            <Link
+              key={link.name}
+              href={link.link}
+              className={
+                active === link.link ? styles.linkClicked : styles.linkUnclicked
+              }
+              onClick={(event) => {
+                event.preventDefault();
+                console.log('active', active, 'link', link.link);
+                setActive(link.link);
+                router.push(link.link);
+                setMenuOpen(false);
+              }}>
+              {link.name}
+            </Link>
+          ))}
+          <Divider color="#3E404E" w={'100%'} />
+          {languages.map((language, index) => (
+            <div
+              className={
+                index === 0 ? styles.languageClicked : styles.languageUnclicked
+              }>
+              {language.name}
+            </div>
           ))}
         </Stack>
       )}
@@ -166,3 +251,6 @@ const Header = (props: IProps) => {
 };
 
 export default Header;
+function useDisclosure(arg0: boolean): [any, { toggle: any }] {
+  throw new Error('Function not implemented.');
+}
