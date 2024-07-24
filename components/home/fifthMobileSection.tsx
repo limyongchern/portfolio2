e99700fc621827ce1da2b1af95b682612db44e94
eddie-vsys <eddie.ling@v.systems>
2024-07-24 15:36:31 +0800

    Fix mobile images

--- components/home/fifthMobileSection.tsx
+++ components/home/fifthMobileSection.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { easeInOut, motion } from 'framer-motion';
 import { Carousel, Embla } from '@mantine/carousel';
 import { Body, BodyBold, Heading } from 'components/typography';
+import { PlanetList } from 'utils/planet';
 
 const FifthMobileSection = ({ data }: any) => {
   const [embla, setEmbla] = useState<Embla | null>(null);
@@ -30,46 +31,57 @@ const FifthMobileSection = ({ data }: any) => {
   };
 
   const slides = data.cards.map((item: any, index: number) => (
+    // const slides = PlanetList.map((item: any, index: number) => (
     <Carousel.Slide key={index}>
       {isActiveSlide(index) ? (
-        <div className={styles.activeCardContainer}>
-          <BodyBold
-            style={{
-              justifyContent: 'center',
-              textAlign: 'center',
-              padding: '0px 50px',
-            }}
-            variant={6}
-            color={'#F2F3F7'}>
-            {item.title}
-          </BodyBold>
-          <Body
-            style={{ textAlign: 'center', marginTop: '10px' }}
-            variant={2}
-            color={'#9198B0'}>
-            {item.description}
-          </Body>
-          <Image src={item.image} alt="" className={styles.activeImageMobile} />
+        <div style={{ paddingTop: '10px' }}>
+          <div className={styles.planetMobileCard}>
+            <div className={styles.widthPlanetMobileCard}>
+              <BodyBold
+                style={{
+                  justifyContent: 'center',
+                  textAlign: 'center',
+                  padding: '0px 50px',
+                }}
+                variant={6}
+                color={'#F2F3F7'}>
+                {item.title}
+              </BodyBold>
+              <Body
+                style={{ textAlign: 'center', marginTop: '10px' }}
+                variant={2}
+                color={'#9198B0'}>
+                {item.description}
+              </Body>
+            </div>
+            <Image
+              src={item.image}
+              alt=""
+              className={styles.activeImageMobile}
+            />
+          </div>
         </div>
       ) : (
-        <div className={styles.nonActiveCardContainer}>
-          <BodyBold
-            style={{
-              justifyContent: 'center',
-              textAlign: 'center',
-              padding: '0px 50px',
-            }}
-            variant={4}
-            color={'#F2F3F7'}>
-            {item.title}
-          </BodyBold>
-          <div style={{ height: '70px' }}>
-            <Body
-              style={{ textAlign: 'center', marginTop: '29.42px' }}
+        <div className={styles.planetCardMobileDeselect}>
+          <div className={styles.widthPlanetMobileCard}>
+            <BodyBold
+              style={{
+                justifyContent: 'center',
+                textAlign: 'center',
+                padding: '0px 50px',
+              }}
               variant={4}
-              color={'#9198B0'}>
-              {item.description}
-            </Body>
+              color={'#F2F3F7'}>
+              {item.title}
+            </BodyBold>
+            <div style={{ height: '70px' }}>
+              <Body
+                style={{ textAlign: 'center', marginTop: '29.42px' }}
+                variant={4}
+                color={'#9198B0'}>
+                {item.description}
+              </Body>
+            </div>
           </div>
           <Image
             src={item.image}
@@ -88,9 +100,10 @@ const FifthMobileSection = ({ data }: any) => {
         style={{ padding: '0px 70px', textAlign: 'center' }}>
         {data.headline}
       </BodyBold>
+
       <motion.div
         className={styles.carouselContainer}
-        initial={{ x: 200, opacity: 1 }}
+        initial={{ x: -100, opacity: 1 }}
         animate={{
           x: 0,
           opacity: 1,
@@ -103,8 +116,8 @@ const FifthMobileSection = ({ data }: any) => {
         }}>
         <Carousel
           // loop
-          slideSize="80%"
-          slideGap={0}
+          slideSize="20%"
+          slideGap={20}
           initialSlide={0}
           className={styles.carouselStyles}
           getEmblaApi={setEmbla}
@@ -112,11 +125,12 @@ const FifthMobileSection = ({ data }: any) => {
           {slides}
         </Carousel>
       </motion.div>
+
       <div
         style={{
           display: 'flex',
           gap: 10,
-          paddingTop: 45,
+          paddingTop: 0,
           justifyContent: 'center',
           alignItems: 'center',
         }}>
